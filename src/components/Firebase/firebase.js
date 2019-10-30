552af10f1d247209fcfe13231888fce8c2945b3f
Mike Stiers <mike.stiers@gmail.com>
2019-10-29 23:09:21 -0400

    signup working and with dotenv

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -8,6 +8,8 @@ const config = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    reactAppAppId: process.env.REACT_APP_APP_ID,
+    reactAppMeasurementID: process.env.REACT_APP_MEASUREMENT_ID,
   };
 
   class Firebase {
